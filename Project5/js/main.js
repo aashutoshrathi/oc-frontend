b0ceb18beb75ea9308b36e29b83dd455d8b5fd2c
Aashutosh Rathi <aashutoshrathi@gmail.com>
2019-07-06 20:57:44 +0530

    Project 5 (#1)
    * Update new font and layout
    
    * Update new font and layout
    
    * :sparkles: Fix menu header centralization
    
    * Create form
    
    * :zap: improvements overall

---
+++ Project5/js/main.js
@@ -0,0 +1,57 @@
+// handleFormSubmission() {
+
+// }
+
+
+function generateRandomQuotes(quantity) {
+    motivationQuotesLength = motivationalQuotes.length;
+    randomNums = generateNUniqueNumbers(quantity, motivationQuotesLength);
+
+    quotes = randomNums.map(num => {
+        return {
+            begin: motivationalQuotes[0][num],
+            mid: motivationalQuotes[1][num],
+            last: motivationalQuotes[2][num],
+            author: motivationalQuotes[3][num]
+        }
+    });
+
+    renderToDOM(quotes);
+}
+
+function renderToDOM(quotes) {
+    quotesDiv = document.getElementById('quotes');
+    quotes.forEach(quote => {
+        const fullQuote = `${quote.begin}${quote.mid}${quote.last}\n~${quote.author}`.trim();
+        quoteDiv = `<div class="uk-card uk-text-center uk-align-center uk-width-1-2@m uk-width-1-1@s uk-box-shadow-large uk-border-rounded uk-card-default uk-card-body">
+                        <blockquote cite="#">
+                            <p class="uk-margin-small-bottom uk-overflow-auto">
+                                ${quote.begin}<br>
+                                ${quote.mid}<br>
+                                ${quote.last}<br>
+                            </p>
+                            <footer>
+                                <cite>
+                                    ${quote.author}
+                                </cite>
+                            </footer>
+                        </blockquote>
+                        <div>
+                            <a class="uk-icon-link" target="_blank" uk-icon="twitter" href="http://twitter.com/share?text=${fullQuote}&url=${document.location}"/>
+                            <a class="uk-icon-link uk-margin-left" target="_blank" uk-icon="whatsapp" href="whatsapp://send?text=${fullQuote}&url=${document.location}"/>
+                        <div>
+                    </div>`;
+        quotesDiv.insertAdjacentHTML("afterbegin", quoteDiv);
+    })
+}
+
+function generateNUniqueNumbers(length, range) {
+    var arr = []
+    while(arr.length < length){
+        var r = Math.floor(Math.random()*range) + 1;
+        if(arr.indexOf(r) === -1) arr.push(r);
+    }
+    return arr;
+}
+
+generateRandomQuotes(1);
---
+++ Project5/js/quoteStore.js
@@ -0,0 +1,36 @@
+const loveQuotes = [
+    [],
+    [],
+    []
+];
+
+const motivationalQuotes = [
+    [
+        " The journey ",
+        " You can't beat ",
+        " The only man ",
+        " If you tell ",
+        " To be your self in the world that"
+    ],
+    [
+        " of a thousand miles ",
+        " the person who ",
+        " who never makes mistakes ",
+        " the truth you",
+        " is constantly trying to make you something else"
+    ],
+    [
+        " begins with a single step.",
+        " never gives up!  ",
+        " is the man who never does anything ",
+        " don't have to remember anything",
+        " is the greatest accomplishment"
+    ],
+    [
+        "Chapter 64 of the Tao Te Ching",
+        "Babe Ruth",
+        "Theodore Roosevelt",
+        "Mark Twain",
+        "Ralph Waldo Emerson"
+    ]
+];
