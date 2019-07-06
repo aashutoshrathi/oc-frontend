c5ec3b524d7255f66e86a0c96921ec8068af35d4
Aashutosh Rathi <aashutoshrathi@gmail.com>
2019-07-07 01:10:48 +0530

    Twitter popup and number input taken

--- Project5/js/main.js
+++ Project5/js/main.js
@@ -1,6 +1,13 @@
-// handleFormSubmission() {
+function handleFormSubmission() {
+    cleanUp();
+    const numberOfQuotes = document.getElementById('form-stacked-select').value;
+    generateRandomQuotes(numberOfQuotes);
+}
 
-// }
+function cleanUp() {
+    const quotesDiv = document.getElementById('quotes');
+    quotesDiv.innerHTML = '';
+}
 
 function generateRandomQuotes(quantity) {
     motivationQuotesLength = motivationalQuotes.length;
@@ -21,7 +28,8 @@ function generateRandomQuotes(quantity) {
 function renderToDOM(quotes) {
     quotesDiv = document.getElementById('quotes');
     quotes.forEach(quote => {
-        const fullQuote = `${quote.begin}${quote.mid}${quote.last}\n~${quote.author}`.trim();
+        const fullQuote = `${quote.begin}${quote.mid}${quote.last} ~${quote.author}`.trim();
+        const twitterLink = `http://twitter.com/share?text=${fullQuote}&url=${document.location}`;
         quoteDiv = `<div class="uk-card uk-text-center uk-align-center uk-width-1-2@m uk-width-1-1@s uk-box-shadow-large uk-border-rounded uk-card-default uk-card-body">
                         <blockquote cite="#">
                             <p class="uk-margin-small-bottom uk-overflow-auto">
@@ -36,9 +44,10 @@ function renderToDOM(quotes) {
                             </footer>
                         </blockquote>
                         <div>
-                            <a class="uk-icon-link" target="_blank" uk-icon="twitter" href="http://twitter.com/share?text=${fullQuote}&url=${document.location}"/>
-                            <a class="uk-icon-link uk-margin-left" target="_blank" uk-icon="whatsapp" href="whatsapp://send?text=${fullQuote}&url=${document.location}"/>
-                        <div>
+                            <a class="uk-icon-link" target="popup" 
+                                onclick="window.open('${twitterLink}','popup','width=600,height=600,scrollbars=no,resizable=no'); return false;" uk-icon="twitter" ></a>
+                            <a class="uk-icon-link uk-margin-left" target="_blank" uk-icon="whatsapp" href="whatsapp://send?text=${fullQuote}&url=${document.location}"></a>
+                        </div>
                     </div>`;
         quotesDiv.insertAdjacentHTML("afterbegin", quoteDiv);
     })
@@ -52,5 +61,3 @@ function generateNUniqueNumbers(length, range) {
     }
     return arr;
 }
-
-generateRandomQuotes(1);
