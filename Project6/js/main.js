242d9b21dd5a2d749d17d13867c655626fd09840
aashutoshrathi <aashutoshrathi@gmail.com>
2019-07-11 11:05:06 +0530

    init P6

---
+++ Project6/js/main.js
@@ -0,0 +1,12 @@
+board = document.querySelector('#board');
+boardSize  = 25;
+
+function generateNUniqueNumbers(length, range) {
+    var arr = []
+    while(arr.length < length){
+        var r = Math.floor(Math.random()*range) + 1;
+        if(arr.indexOf(r) === -1) arr.push(r);
+    }
+    return arr;
+}
+
