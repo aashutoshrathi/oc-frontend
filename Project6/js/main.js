d6196b7a5894b26731bb9133484a48ea4ad3ec56
Aashutosh Rathi <aashutoshrathi@gmail.com>
2019-07-31 16:12:53 +0530

    Upload images

--- Project6/js/main.js
+++ Project6/js/main.js
@@ -1,6 +1,3 @@
-board = document.querySelector('#board');
-boardSize  = 10;
-
 function generateNUniqueNumbers(length, range) {
     var arr = []
     while(arr.length < length){
@@ -10,10 +7,40 @@ function generateNUniqueNumbers(length, range) {
     return arr;
 }
 
-for(let i=0; i<10; i++) {
-    board.innerHTML += `<div class="row" id="row-${i+1}"></div>`
-    row = document.querySelector(`#row-${i+1}`);
-    for(let j=0; j<10; j++) {
-        row.innerHTML += `<div class="box" id="box-${(i)*10 + j+1}"></div>`
+function initEmptyBoard() {
+    board = document.querySelector('#board');
+    boardSize  = 10;
+    
+    for(let i=0; i<10; i++) {
+        board.innerHTML += `<div class="row" id="row-${i+1}"></div>`
+        row = document.querySelector(`#row-${i+1}`);
+        for(let j=0; j<10; j++) {
+            row.innerHTML += `<div class="box" id="box-${(i)*10 + j+1}"></div>`
+        }
     }
 }
+
+function addHurdles(hurdles) {
+    hurdles.forEach(hurdle => {
+        targetBox = document.querySelector(`#box-${hurdle}`);
+        targetBox.innerHTML = `<img href="aashutoshrathi.tk/oc-frontend/Project6/images/tree.png"></img>`;
+    });
+}
+
+function addWeapons(weapons) {
+    weapons.forEach(weapon => {
+        targetBox = document.querySelector(`#box-${weapon}`);
+        targetBox.innerText = 'W';
+    });
+}
+
+function renderBoard() {
+    initEmptyBoard();
+    randomNumbers = generateNUniqueNumbers(20, 100);
+    hurdles = randomNumbers.slice(0, 10);
+    weapons = randomNumbers.slice(10, 20);
+    addHurdles(hurdles);
+    addWeapons(weapons);
+}
+
+renderBoard();
