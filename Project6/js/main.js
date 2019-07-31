37100342a07f1d23ac6c363ca9e3d57a6189fcec
Aashutosh Rathi <aashutoshrathi@gmail.com>
2019-07-31 17:24:10 +0530

    Add weapon imageS

--- Project6/js/main.js
+++ Project6/js/main.js
@@ -1,3 +1,35 @@
+const imageDir = `https://aashutoshrathi.tk/oc-frontend/Project6/images/`;
+
+const hurdleBlock = `<img src="${imageDir}tree.png"></img>`;
+
+const weaponMap = [
+    {
+        image: `${imageDir}alpha.png`,
+        score: 10,
+        name: 'alpha'
+    },
+    {
+        image: `${imageDir}beta.png`,
+        score: 20,
+        name: 'beta'
+    },
+    {
+        image: `${imageDir}gamma.png`,
+        score: 30,
+        name: 'gamma'
+    },
+    {
+        image: `${imageDir}delta.png`,
+        score: 40,
+        name: 'delta'
+    },
+    {
+        image: `${imageDir}pi.png`,
+        score: 50,
+        name: 'pi'
+    }
+];
+
 function generateNUniqueNumbers(length, range) {
     var arr = []
     while(arr.length < length){
@@ -23,24 +55,36 @@ function initEmptyBoard() {
 function addHurdles(hurdles) {
     hurdles.forEach(hurdle => {
         targetBox = document.querySelector(`#box-${hurdle}`);
-        targetBox.innerHTML = `<img href="aashutoshrathi.tk/oc-frontend/Project6/images/tree.png"></img>`;
+        targetBox.innerHTML = hurdleBlock;
     });
 }
 
 function addWeapons(weapons) {
     weapons.forEach(weapon => {
         targetBox = document.querySelector(`#box-${weapon}`);
-        targetBox.innerText = 'W';
+        const randomWeaponIndex = Math.floor(Math.random()*(weaponMap.length)) + 1;
+        const { image, name, score } = weaponMap[randomWeaponIndex];
+        targetBox.innerHTML = `<img src="${image}" alt="${name}">${score}</img>`;
     });
 }
 
 function renderBoard() {
     initEmptyBoard();
-    randomNumbers = generateNUniqueNumbers(20, 100);
+    randomNumbers = generateNUniqueNumbers(22, 100);
     hurdles = randomNumbers.slice(0, 10);
     weapons = randomNumbers.slice(10, 20);
+
+    playerOnePosition = randomNumbers.slice(20, 21);
+    playerTwoPosition = randomNumbers.slice(21, 22);
+
     addHurdles(hurdles);
     addWeapons(weapons);
 }
+
+// class Weapon(i) {
+//     type = i
+//     power = weaponMap[i].power
+//     image = weaponMap[i].image
+// }
 
 renderBoard();
